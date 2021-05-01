5b7be7f86fb3f59eef12ac6672868eb16810c909
Johan Sánchez <johan@users-MacBook-Pro.local>
2021-05-01 16:18:48 -0500

    ajuste enviroment

--- server/config/config.js
+++ server/config/config.js
@@ -13,10 +13,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://jmsm157:QORgX93Jvh2DuAPk@cluster0.lfrqd.mongodb.net/cafe?retryWrites=true&w=majority'
-    // }
+if (process.env.NODE_ENV === 'dev') {
+    // urlDB = 'mongodb://localhost:27017/cafe';
+    urlDB = 'mongodb+srv://jmsm157:QORgX93Jvh2DuAPk@cluster0.lfrqd.mongodb.net/cafe?retryWrites=true&w=majority';
+} else {
+    urlDB = 'mongodb+srv://jmsm157:QORgX93Jvh2DuAPk@cluster0.lfrqd.mongodb.net/cafe?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
