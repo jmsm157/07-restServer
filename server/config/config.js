2c7d597632c1a98dd02d17ca354cce2b70cf8f6e
Johan Sánchez <johan@users-MacBook-Pro.local>
2021-05-01 15:30:52 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 //=================================
 process.env.PORT = process.env.PORT || 3000;
+
+//=================================
+// Entorno
+//=================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=================================
+// Base de datos
+//=================================
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://jmsm157:QORgX93Jvh2DuAPk@cluster0.lfrqd.mongodb.net/cafe?retryWrites=true&w=majority'
+    // }
+
+process.env.URLDB = urlDB;
